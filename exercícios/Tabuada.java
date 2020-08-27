3e75874c40bef8791e1db582a0facc3b9f760065
TiagoIkenaga <tiagoikenaga@gmail.com>
2020-08-27 04:08:16 -0300

    Update Tabuada.java
    implementação do mesmo programa mas utilizando laço de repetição tipo do while (faça enquanto), ou seja enquanto a condição for verdadeira ele vai repetir

--- exercícios/Tabuada.java
+++ exercícios/Tabuada.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 public class Tabuada {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n;
+        int n,c=1;
         
         System.out.print("Digite um número: ");
         n = scanner.nextInt();
         System.out.println();
         System.out.println("Tabuada do "+ n);
         
-        for (int c = 1; c < 11; c++) {
+        do {
             System.out.println(n +" x "+ c +" = "+ (n * c));
-        }
+            c++;           
+        } while (c<11);
         System.out.println();
         scanner.close();
     }
