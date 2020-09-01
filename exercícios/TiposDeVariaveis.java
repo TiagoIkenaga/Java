f51fe62358c829b23651c780225d26b18e3e8a5c
TiagoIkenaga <tiagoikenaga@gmail.com>
2020-09-01 20:29:09 -0300

    Create TiposDeVariaveis.java
    alguns tipos de variáveis no Java

---
+++ exercícios/TiposDeVariaveis.java
@@ -0,0 +1,16 @@
+// Exemplo de tipos de variáveis
+// TIPO identificador = Expressão ;
+
+public class TiposDeVariaveis {
+    public static void main(String[] args) {
+        int anoFabricacao = 2010;
+        float velocidade = 100.05f; // f para indicar float
+        char letra = 'a'; // '' para identificar um caracter
+        boolean licenciado = false; // variáveis booleanas só guardam o valor true(verdadeiro) ou false(falso)
+        
+        System.out.println(anoFabricacao);
+        System.out.println(velocidade);
+        System.out.println(letra);
+        System.out.println(licenciado);
+    }
+}
