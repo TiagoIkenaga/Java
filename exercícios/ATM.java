87679a595c961a9e65d6b426217da9da455db7f7
TiagoIkenaga <tiagoikenaga@gmail.com>
2020-09-01 20:51:06 -0300

    Update ATM.java
    fatorando o mesmo programa e "enxugando(diminuindo" as linhas de código

--- exercícios/ATM.java
+++ exercícios/ATM.java
@@ -1,29 +1,17 @@
 public class ATM {
     public static void main(String[] args) {
-        int originalAmount = 12345;
+        int originalAmount = 1245;
         int amount = originalAmount;
 
-        int n100 = amount/100;
-        amount %= 100;// amount = amount % 100
-        int n50 = amount/50;
-        amount %= 50;// amount = amount % 50
-        int n20 = amount/20;
-        amount %= 20;// amount = amount % 20
-        int n10 = amount/10;
-        amount %= 10;// amount = amount % 10
-        int n5 = amount/5;
-        amount %= 5;// amount = amount % 5
-        int n2 = amount/2;
-        amount %= 2;// amount = amount % 2
-        int n1 = amount;
+        System.out.println("Dividir R$"+originalAmount+" em notas de R$100, R$50, R$20, R$10, R$5, R$2, R$1");
 
-        System.out.println("Notas de R$100,00: "+n100);
-        System.out.println("Notas de R$50,00: "+n50);
-        System.out.println("Notas de R$20,00: "+n20);
-        System.out.println("Notas de R$10,00: "+n10);
-        System.out.println("Notas de R$5,00: "+n5);
-        System.out.println("Notas de R$2,00: "+n2);
-        System.out.println("Notas de R$1,00: "+n1);
-        System.out.println("Resto: "+amount);
+        int[] billValues = {100,50,20,10,5,2,1};
+        int[] numberOfBills = new int[billValues.length];
+
+        for (int i = 0; i < billValues.length; i++) {
+            numberOfBills[i] = amount / billValues[i];
+            amount %= billValues[i];
+            System.out.println("Notas de R$"+billValues[i]+": "+numberOfBills[i]);
+        }
     }
 }
