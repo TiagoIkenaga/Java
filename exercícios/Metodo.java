0406da787f15f57cec79549c2f77fc0bfa1cdf06
TiagoIkenaga <tiagoikenaga@gmail.com>
2020-09-01 23:31:04 -0300

    Update Metodo.java
    atualizando o exemplo

--- exercícios/Metodo.java
+++ exercícios/Metodo.java
@@ -1,17 +1,16 @@
 //Métodos servem pra dividir blocos de funcionalidade do código
 
 public class Metodo {
-    public static void main(String[] args) {
-        
-        float calculaVelocidadeAtual(long tAtual, float vAtual){
-            float velocidade = (vAtual-velocidadeAnterior)/(tAtual-tempoAnterior);
-            velocidadeAnterior = vAtual;
-            tempoAnterior = tAtual;
-            return velocidade;
-        }// calcula a velocidade Atual
+    // public static void main(String[] args) {      
 
-        if (calculaVelocidadeAtual(tempoAtual, velocidadeAtual) > 80) {
-            velocidadeAtual = velocidadeAtual-1f;// diminui a velocidade atual
-        }// excesso de velocidade
-    }
+    //     if (calculaVelocidadeAtual(tempoAtual, velocidadeAtual) > 80) {
+    //         velocidadeAtual = velocidadeAtual-1f;// diminui a velocidade atual
+    //     }// excesso de velocidade
+    // }
+    // public static float calculaVelocidadeAtual(long tAtual, float vAtual){
+    //     float velocidade = (vAtual-velocidadeAnterior)/(tAtual-tempoAnterior);
+    //     velocidadeAnterior = vAtual;
+    //     tempoAnterior = tAtual;
+    //     return velocidade;
+    // }// calcula a velocidade Atual
 }
