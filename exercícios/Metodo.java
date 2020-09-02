6d6aeeb0a595ac1d0b034e5405dfe782da666b13
TiagoIkenaga <tiagoikenaga@gmail.com>
2020-09-01 21:08:40 -0300

    Create Metodo.java
    exemplo de métodos no Java

---
+++ exercícios/Metodo.java
@@ -0,0 +1,17 @@
+//Métodos servem pra dividir blocos de funcionalidade do código
+
+public class Metodo {
+    public static void main(String[] args) {
+        
+        float calculaVelocidadeAtual(long tAtual, float vAtual){
+            float velocidade = (vAtual-velocidadeAnterior)/(tAtual-tempoAnterior);
+            velocidadeAnterior = vAtual;
+            tempoAnterior = tAtual;
+            return velocidade;
+        }// calcula a velocidade Atual
+
+        if (calculaVelocidadeAtual(tempoAtual, velocidadeAtual) > 80) {
+            velocidadeAtual = velocidadeAtual-1f;// diminui a velocidade atual
+        }// excesso de velocidade
+    }
+}
